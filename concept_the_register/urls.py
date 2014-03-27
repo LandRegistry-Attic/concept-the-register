0c71f0ccbb87b64d9e83ac036d65f68bceb6f54f
vagrant <vagrant@packer-virtualbox-iso.(none)>
2014-03-27 13:44:32 +0000

    add Django REST API support

--- concept_the_register/urls.py
+++ concept_the_register/urls.py
@@ -1,12 +1,24 @@
-from django.conf.urls import patterns, include, url
+from django.conf.urls import url, patterns, include
+from django.contrib.auth.models import User, Group
+from rest_framework import viewsets, routers
 
-from django.contrib import admin
-admin.autodiscover()
+# ViewSets define the view behavior.
+class UserViewSet(viewsets.ModelViewSet):
+    model = User
 
+class GroupViewSet(viewsets.ModelViewSet):
+    model = Group
+
+
+# Routers provide an easy way of automatically determining the URL conf.
+router = routers.DefaultRouter()
+router.register(r'users', UserViewSet)
+router.register(r'groups', GroupViewSet)
+
+
+# Wire up our API using automatic URL routing.
+# Additionally, we include login URLs for the browseable API.
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'concept_the_register.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^', include(router.urls)),
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 )
